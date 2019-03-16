b72e6f26463f9382bb27296e54b847054578b4aa
Jose Carlos De La Colina <josecarlos@Joses-MacBook-Pro.local>
2019-03-16 14:49:35 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,31 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+cards =["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+cardsInPlay =[];
+
+var cardOne = cards[1];
+var cardTwo = cards[2];
+
+
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped queen");
+console.log("User flipped king");
+
+
+/*if (cardsInPlay === cardOne) {
+	console.log("User flipped queen");
+}
+else (cardsInPlay === cardTwo) { 
+	console.log("User flipped king");
+}*/
+
+if (cardsInPlay.length === 2 && cardOne === cardTwo) {
+	alert("You found a match!");
+}
+else { 
+	alert("Sorry, try again.");
+}
